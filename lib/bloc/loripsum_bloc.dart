44d82470e73b8d0f0e978cabaed33d89a0d3f74f
Luiz Roberto <passageiro1829@gmail.com>
2022-02-18 20:47:46 -0300

    dentro da CarroPage foi criado menu popUp, adicionado descricao e icones para favorito e share. criei metodo global para Text,busca de  api Loripsum e manipulando-a com streamBuilder e bloc, por fim criei chache para APi Loripsum

---
+++ lib/bloc/loripsum_bloc.dart
@@ -0,0 +1,20 @@
+import 'dart:async';
+
+import 'package:projeto_carros/api/loripsum_api.dart';
+
+class LoripsumBloc {
+  static String? lorin;
+  final _streaamController = StreamController<String>();
+
+  Stream<String> get stream => _streaamController.stream;
+
+  fatch() async {
+    String s = lorin! ?? await LoripsumApi.getLoripsum();
+    lorin = s;
+    _streaamController.add(s);
+  }
+
+  void dispose() {
+    _streaamController.close();
+  }
+}
--- lib/pages/carro/carro_page.dart
+++ lib/pages/carro/carro_page.dart
@@ -1,16 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:projeto_carros/api/loripsum_api.dart';
+import 'package:projeto_carros/bloc/loripsum_bloc.dart';
 import 'package:projeto_carros/models/carro.dart';
+import 'package:projeto_carros/widgets/text.dart';
 
-class CarroPage extends StatelessWidget {
+//criando menu popUp, adicionando descricao e icones para favorito e share, metodo global para Text,buscando api Loripsum e manipulando-a com streamBuilder e bloc
+class CarroPage extends StatefulWidget {
   Carro carro;
 
   CarroPage(this.carro);
 
   @override
+  State<CarroPage> createState() => _CarroPageState();
+}
+
+class _CarroPageState extends State<CarroPage> {
+  final _loripsumApiBloc = LoripsumBloc();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _loripsumApiBloc.fatch();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("${carro.nome}"),
+        title: Text("${widget.carro.nome}"),
+        actions: [
+          IconButton(
+            onPressed: _onClickMapa,
+            icon: Icon(Icons.place),
+          ),
+          IconButton(
+            onPressed: _onClickVideo,
+            icon: Icon(Icons.videocam),
+          ),
+          PopupMenuButton<String>(
+            onSelected: _onClickPopUpMenu,
+            itemBuilder: (context) {
+              return [
+                PopupMenuItem(child: Text("Editar"), value: "Editar"),
+                PopupMenuItem(child: Text("Deletar"), value: "Deletar"),
+                PopupMenuItem(child: Text("Share"), value: "Share"),
+              ];
+            },
+          ),
+        ],
       ),
       body: _body(),
     );
@@ -19,7 +57,99 @@ class CarroPage extends StatelessWidget {
   _body() {
     return Container(
       padding: EdgeInsets.all(16),
-      child: Image.network("${carro.urlFoto}"),
+      child: ListView(
+        children: [
+          Image.network("${widget.carro.urlFoto}"),
+          _bloco1(),
+          Divider(),
+          _bloco2(),
+        ],
+      ),
     );
+  }
+
+  Row _bloco1() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            text("${widget.carro.nome}", fontSize: 20, bold: true),
+            text("${widget.carro.tipo}", fontSize: 16),
+          ],
+        ),
+        Row(
+          children: [
+            IconButton(
+                onPressed: _onClickFavorito,
+                icon: Icon(
+                  Icons.favorite,
+                  color: Colors.red,
+                  size: 40,
+                )),
+            IconButton(
+                onPressed: _onClickShare,
+                icon: Icon(
+                  Icons.share,
+                  size: 40,
+                )),
+          ],
+        ),
+      ],
+    );
+  }
+
+  _bloco2() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        text("${widget.carro.descricao}", fontSize: 14, bold: true),
+        SizedBox(
+          height: 20,
+        ),
+        StreamBuilder<String>(
+          stream: _loripsumApiBloc.stream,
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            return text(snapshot.data!, fontSize: 16);
+          },
+        ),
+        text(""),
+      ],
+    );
+  }
+
+  void _onClickMapa() {}
+
+  void _onClickVideo() {}
+
+  _onClickPopUpMenu(String value) {
+    switch (value) {
+      case "Editar":
+        print("Editar!!!");
+        break;
+      case "Deletar":
+        print("Deletar!!!");
+        break;
+      case "Share":
+        print("Share!!!");
+        break;
+    }
+  }
+
+  void _onClickFavorito() {}
+
+  void _onClickShare() {}
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _loripsumApiBloc.dispose();
   }
 }
